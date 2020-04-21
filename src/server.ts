238ef9ee4af6f7da30981b1dca88325886594fcc
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-04-21 17:36:31 -0300

    Appointments com o TypeORM funcionando

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes';
+
+import './database/index';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333');
+});
