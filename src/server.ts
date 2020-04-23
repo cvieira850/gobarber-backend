2e6ec1e4fda228a56c61985eed22b27425551191
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-04-23 20:54:24 -0300

    Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadCOnfig from './config/upload';
 
 import './database/index';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadCOnfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
