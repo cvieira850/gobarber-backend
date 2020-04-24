38c98a9088b10ece80caf47ca1836f940398f7cd
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-04-23 21:04:51 -0300

    Criando classe de erro

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -17,7 +17,7 @@ sessionssRouter.post('/', async (request, response) => {
     delete user.password;
     return response.json({ user, token });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(err.statusCode).json({ error: err.message });
   }
 });
 
