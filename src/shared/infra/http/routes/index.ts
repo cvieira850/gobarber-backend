e6c9cdba1e6b90b74d3fed5cd1d57c9434478756
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-26 21:12:36 -0300

    Adding Lyskov Substitution Principle on project

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionssRouter from '@modules/users/infra/http/routes/sessions.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionssRouter);
+
+export default routes;
