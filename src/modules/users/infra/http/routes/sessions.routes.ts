ed9dce4f66f7ce26bbf8f5a6812d33ac11ce8e19
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-27 21:27:15 -0300

    Adding Dependency Inversion Principle

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const sessionssRouter = Router();
 
 sessionssRouter.post('/', async (request, response) => {
+  const usersRepository = new UsersRepository();
   const { email, password } = request.body;
 
-  const authenticateUserService = new AuthenticateUserService();
+  const authenticateUserService = new AuthenticateUserService(usersRepository);
 
   const { user, token } = await authenticateUserService.execute({
     email,
